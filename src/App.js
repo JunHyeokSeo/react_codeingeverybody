90f5aececdfde9d3dc091c43c007be792cf7b05f
JunHyeok Seo <sjhgd107@naver.com>
2023-07-28 15:48:13 +0900

    리액트는 사용자 정의 태그를 만드는 기술이다. 사용자 정의 태그는 컴포넌트라고 부른다. 컴포넌트는 함수다. 함수의 리턴값이 컴포넌트의 UI가 된다. 컴포넌트의 이름은 반드시 대문자로 시작한다. 리턴값은 하나의 태그로 시작해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
+//단일 값 반환을 위해, div로 묶음
+function Counter(){
+  return <div>
+    <h1>Counter</h1>
+    <button>+</button> 0
+  </div>
+}
+
 function App() {
   return (
     <div>
-      <h1>Counter</h1>
-      <button>+</button> 0
+      <Counter></Counter>
     </div>
   );
 }
