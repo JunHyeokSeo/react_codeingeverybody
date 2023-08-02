e4b242b7bc5a2630ec42662f408387f27781ee2a
JunHyeok Seo <sjhgd107@naver.com>
2023-08-02 14:28:52 +0900

    리액트의 코드는 js가 아니라 jsx이다. jsx는 Js+태그를 쉽게 코드에 포함시킬 수 있도록 고안된 js 확장 언어이다
    onclick -> onClick
    onchange -> onChange
    
    이벤트의 값은 함수다
    
    props의 값은 read only
    State의 값은 쓰기 가능
    
    state는 배열이다.
    첫번째 원소는 값이다. 읽을 때 쓴다. 수정하면 안된다.
    두번째 원소는 함수다. 수정할 때 호출한다. 파라미터가 변경되었을 때만 컴포넌트가 다시 실행된다
    
    props, state가 바뀌면 컴포넌트가 다시 렌더링 된다

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 
 //단일 값 반환을 위해, div로 묶음
 //변수로서 사용하기 위해 {} 중괄호 사용
 //컴포넌트를 함수로 표현, 과거에는 클래스로 표현
+//컴포넌트는 외부에서 내부로 주입되는 상태, 스테이트는 내부적으로 사용하는 상태
+//컴포넌트 구현부 내부에서 자의적으로 props 값 변경 불가
+
+//상태는 배열이다.
+//배열의 첫번째 원소는 상태의 값이다 (읽을 때 사용)
+//두번째 원소는 상태의 값을 변경할 때 호출하는 함수다 (바꿀 때 사용)
+//리액트는 setCount() 값이 이전 값과 다르다면 컴포넌트를 재실행 한다.
+
+//state 미사용시 프로퍼티스의 값을 변경할수는 있지만, 컴포넌트를 다시 랜더링 할 수 없다
 function Counter(props){
+	let countState = useState(props.initValue);
+	let count = countState[0];
+	let setCount = countState[1];
+	console.log(countState);
+	function up(){
+		console.log(1);
+		setCount(count + 1);
+	}
 	console.log(props.title);
 	return <div>
 		<h1>{props.title}</h1>
-		<button>+</button> {props.initValue}
+		<button onClick={up}>+</button> {count}
 	</div>
 }
 //함수와 태그를 등가관계에 놓고 태그를 함수로 표현
@@ -17,8 +35,7 @@ function Counter(props){
 function App() {
   return (
 	<div>
-		<Counter title="불면증 카운터" initValue = {10}></Counter>
-		<Counter title="손님 카운터" initValue = {10}></Counter>
+		<Counter title="불면증 카운터" initValue = {0}></Counter>
 	</div>
   );
 }
