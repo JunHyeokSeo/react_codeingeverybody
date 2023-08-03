db85272d7714a9b813735864fbf5de2487e8bf82
JunHyeok Seo <sjhgd107@naver.com>
2023-08-03 10:06:05 +0900

    구조 분해 할당을 이용해서 코드를 보다 간결하고, 분명하게 개선

--- src/App.js
+++ src/App.js
@@ -13,21 +13,32 @@ import { useState } from 'react';
 //리액트는 setCount() 값이 이전 값과 다르다면 컴포넌트를 재실행 한다.
 
 //state 미사용시 프로퍼티스의 값을 변경할수는 있지만, 컴포넌트를 다시 랜더링 할 수 없다
-function Counter(props){
-	let countState = useState(props.initValue);
-	let count = countState[0];
-	let setCount = countState[1];
-	console.log(countState);
+
+// 컴포넌트가 무수히 많다면, props 객체에 어떤 값이 담겨서 오는지, 어떤 값을 입력해야 하는지 확인하기 어려움
+// 아래와 같이 props의 값을 상수 형태로 저장해두면 props 객체 내부의 값을 파악하기 쉬움
+// function Counter(props) {
+//   const title = props.title;
+//   const initValue = props.initValue;
+
+// 위와 같은 코드를 컴포넌트 선언부 한줄로 줄일 수 있다
+// 이러한 개념을 '구조 분해 할당'이라고 한다
+function Counter({ title, initValue }){
+	//상수로 선언하여, 직접 수정 시 에러 발생
+	// const countState = useState(initValue);
+	// const count = countState[0];
+	// const setCount = countState[1];
+
+	//위 코드를 아래 한줄로 변경 - '구조 분해 할당'
+	const [count, setCount] = useState(initValue);
 	function up(){
-		console.log(1);
 		setCount(count + 1);
 	}
-	console.log(props.title);
-	return <div>
-		<h1>{props.title}</h1>
+	return (<div>
+		<h1>{title}</h1>
 		<button onClick={up}>+</button> {count}
-	</div>
+	</div>);
 }
+
 //함수와 태그를 등가관계에 놓고 태그를 함수로 표현
 //props는 입력값이다.
 //html에서는 태그 안의 값을 속성이라고 하고,
